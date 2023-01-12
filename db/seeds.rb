a528527a1510f12de4626fec05df63ef24285725
Jake Wehder <jw@email.comjake>
2023-01-11 18:54:43 -0600

    creating models for my tables;

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+first_names = ["Mark", "John", "Luke", "Jake", "Tom", "Sandy", "Jan", "Jennifer", "Julia", "Pam", "Greg", "Liz"]
+
+last_names = ["Williams", "Wehder", "Johnson", "James", "Carlucci", "Rowell", "Parris", "Stephens", "Verstappen", "Perez", "Samuels", "Wyatt"]
+
+Salespeopl
 
 puts "✅ Done seeding!"
+
