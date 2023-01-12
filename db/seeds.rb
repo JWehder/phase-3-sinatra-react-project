aadcdebadb6963a55b20b82c0f0dd044a98292f6
Jake Wehder <jw@email.comjake>
2023-01-11 19:17:54 -0600

    modifying quota for each salesperson. adding more customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ first_names = ["Mark", "John", "Luke", "Jake", "Tom", "Sandy", "Jan", "Jennifer"
 
 last_names = ["Williams", "Wehder", "Johnson", "James", "Carlucci", "Rowell", "Parris", "Stephens", "Verstappen", "Perez", "Samuels", "Wyatt"]
 
-6.times do
-    Salesperson.create(first_name: first_names.sample, last_name: last_names.sample, quota: rand(10000..25000))
+Salesperson.all.each do
+    Salesperson.update(quota: rand(140000..200000))
 end
 
 Salesperson.all.each do |salesperson|
