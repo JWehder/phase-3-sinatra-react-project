1d90b35fbb02011da2c720d71804159150bc1146
Jake Wehder <jw@email.comjake>
2023-01-12 16:18:41 -0600

    added faker to generate names for the customers and salespeople

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  get "/salespeople" do
+    salespeople = Salesperson.all.order(:first_name)
+    salespeople.to_json(include: :customers)
+  end
+
   get "/customers" do
-    Customer.all.to_json
+    customers = Customer.all.order(revenue: :desc)
+    customers.to_json(include: :salesperson)
   end
 
 end
