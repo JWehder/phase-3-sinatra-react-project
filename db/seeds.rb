f150ee46a96dfdf84cdf670a4dfdd739e3e4dfa4
Jake Wehder <jw@email.comjake>
2023-01-11 19:06:45 -0600

    finished creating seed data. Beginning seeding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,15 @@ first_names = ["Mark", "John", "Luke", "Jake", "Tom", "Sandy", "Jan", "Jennifer"
 
 last_names = ["Williams", "Wehder", "Johnson", "James", "Carlucci", "Rowell", "Parris", "Stephens", "Verstappen", "Perez", "Samuels", "Wyatt"]
 
-Salespeopl
+6.times do
+    Salesperson.create(first_name: first_names.sample, last_name: last_names.sample, quota: rand(10000..25000))
+end
+
+Salesperson.all.each do |salesperson|
+    4.times do
+        Customer.create(salesperson_id: salesperson.id, customer_first_name: first_names.sample, customer_last_name: last_names.sample, units_sold: rand(50..100), revenue: rand(1000..60000))
+    end
+end
 
 puts "✅ Done seeding!"
 
