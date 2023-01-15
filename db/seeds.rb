cf35ddcf29e291a83b1a101ab6f53778863dbb65
Jake Wehder <jw@email.comjake>
2023-01-15 12:53:16 -0600

    adding routes for specific salespeople to backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,14 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-first_names = ["Mark", "John", "Luke", "Jake", "Tom", "Sandy", "Jan", "Jennifer", "Julia", "Pam", "Greg", "Liz"]
-
-last_names = ["Williams", "Wehder", "Johnson", "James", "Carlucci", "Rowell", "Parris", "Stephens", "Verstappen", "Perez", "Samuels", "Wyatt"]
 
 6.times do
-    Salesperson.create(first_name: first_names.sample, last_name: last_names.sample, quota: rand(10000..25000))
+    Salesperson.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, quota: rand(10000..25000))
 end
 
 Salesperson.all.each do |salesperson|
     4.times do
-        Customer.create(salesperson_id: salesperson.id, customer_first_name: first_names.sample, customer_last_name: last_names.sample, units_sold: rand(50..100), revenue: rand(1000..60000))
+        Customer.create(salesperson_id: salesperson.id, customer_first_name: Faker::Name.first_name, customer_last_name: Faker::Name.last_name, units_sold: rand(50..100), revenue: rand(1000..60000))
     end
 end
 
