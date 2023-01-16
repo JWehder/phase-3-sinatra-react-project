af56c1b239f318a6d51d42ac5c18cf0d7d5bf130
Jake Wehder <jw@email.comjake>
2023-01-16 14:07:18 -0600

    creating a delete request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,9 +36,14 @@ class ApplicationController < Sinatra::Base
   patch '/customers/:id' do 
     customer = Customer.find(params[:id])
     customer.update(
-      salesperson_id: params[:salesperson_id]
+      salesperson_id: params[:salesperson_id],
+      customer_first_name: params[:customer_first_name],
+      customer_last_name: params[:customer_last_name],
+      units_sold: params[:units_sold],
+      revenue: params[:revenue]
     )
-
+    all_customers = Customer.all.order(revenue: :desc)
+    all_customers.to_json(include: :salesperson)
   end
 
 end
