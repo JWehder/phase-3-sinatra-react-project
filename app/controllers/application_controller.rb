75cd13ed32dd2fc2ad68ba8cbf6339134d4c5349
Jake Wehder <jw@email.comjake>
2023-02-04 19:14:33 -0600

    getting rid of nested salespeople within customers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < Sinatra::Base
     salespeople.to_json(include: { customers: { include: :salesperson } })
   end
 
-  get "/customers" do
-    customers = Customer.all.order(revenue: :desc)
-    customers.to_json(include: :salesperson)
-  end
+  # get "/customers" do
+    # customers = Customer.all.order(revenue: :desc)
+    # customers.to_json(include: :salesperson)
+  # end
 
   get "/salespeople/:id" do
     salesperson = Salesperson.find(params[:id])
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
       units_sold: params[:units_sold],
       revenue: params[:revenue]
     )
-    new_customer.salesperson.to_json
+    salesperson.to_json
   end
 
   get '/customers/:id' do
@@ -34,16 +34,18 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/customers/:id' do 
+    # should I use a similar method as my post or just find customer by ID?
     customer = Customer.find(params[:id])
-    customer.update(
+    edited_customer = customer.update(
       salesperson_id: params[:salesperson_id],
       customer_first_name: params[:customer_first_name],
       customer_last_name: params[:customer_last_name],
       units_sold: params[:units_sold],
       revenue: params[:revenue]
     )
-    all_customers = Customer.all.order(revenue: :desc)
-    all_customers.to_json(include: :salesperson)
+    # all_customers = Customer.all.order(revenue: :desc)
+    # all_customers.to_json(include: :salesperson)
+    edited_customer.salesperson.to_json
   end
 
   delete '/customers/:id' do
